cf0acb0a75c420b64b3d9108f91de83cf9c01c22
salmanyousaf240 <salmanyousaf240@gmail.com>
2020-05-03 20:39:58 +0500

    page not found added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/shared/Navbar";
-import Users from "./components/user/Users";
-import Search from "./components/Search";
 import Alert from "./components/Alert";
 import About from "./components/pages/About";
 import User from "./components/user/User";
 
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 const App = () => {
   return (
@@ -21,18 +21,10 @@ const App = () => {
             <div className="container">
               <Alert alert={alert} />
               <Switch>
-                <Route
-                  path="/"
-                  exact
-                  render={() => (
-                    <>
-                      <Search />
-                      <Users />
-                    </>
-                  )}
-                />
+                <Route path="/" exact component={Home} />
                 <Route path="/about" exact component={About} />
                 <Route path="/users/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "../Search";
+import Users from "../user/Users";
+
+const Home = () => {
+  return (
+    <>
+      <Search />
+      <Users />
+    </>
+  );
+};
+
+export default Home;
