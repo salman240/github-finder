ff5b308f9a9678f0e87cf5cb4baeeb2ca668f1f9
salman240 <salmanyousaf240@gmail.com>
2020-10-10 14:49:51 +0500

    debugging node_env

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -16,9 +16,19 @@ let githubClientSecret;
 if (process.env.NODE_ENV !== "production") {
   githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
   githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+
+  console.log('client');
+  console.log(githubClientId);
+  console.log(githubClientSecret);
+  console.log(process.env.NODE_ENV);
 } else {
   githubClientId = process.env.GITHUB_CLIENT_ID;
   githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+  console.log('prod');
+  console.log(githubClientId);
+  console.log(githubClientSecret);
+  console.log(process.env.NODE_ENV);
 }
 
 const GithubState = (props) => {
